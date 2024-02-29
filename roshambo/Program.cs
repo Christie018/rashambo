061c45b6b37359c6482c87ae0551857610d77ace
Christie018 <christie.sullivan018@gmail.com>
2024-02-28 22:14:18 -0500

    Initial commit

---
+++ roshambo/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Rashambo Rock Paper Scissors 
+ * Mattalynn Darden
+ * 2/28/24 */
+
+// The application prompts the player to enter a name and select an opponent.
+
+
+//The application prompts the player to select rock, paper, or scissors. Then, the application displays the player’s choice, the opponent’s choice, and the result of the match.
+
+
+// The application continues until the user doesn’t want to play anymore.
+
+
+//If the user makes an invalid selection, the application should display an appropriate error message and prompt the user again until the user makes a valid selection.
